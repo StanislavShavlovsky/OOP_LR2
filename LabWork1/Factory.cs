c8bc8dab04fa1b27b3ca2047ca8a358c4a4194e6
Shaulouski_Stanislau <Shavlovsijstanislav3211@mail.ru>
2026-02-27 19:10:24 +0100

    Создание класса Factory

---
+++ LabWork1/Factory.cs
@@ -0,0 +1,45 @@
+﻿public class Factory
+{
+    private int _id;                     
+    private string _name;                
+    private string _location;            
+    private int _establishedYear;        
+
+    public Factory(int id, string name, string location, int establishedYear)
+    {
+        _id = id;
+        _name = name;
+        _location = location;
+        _establishedYear = establishedYear;
+    }
+
+    public int GetId() => _id;
+    public string GetName() => _name;
+
+    public void SetName(string name)
+    {
+        if (!string.IsNullOrWhiteSpace(name))
+            _name = name;
+    }
+
+    public string GetLocation() => _location;
+
+    public void SetLocation(string location)
+    {
+        if (!string.IsNullOrWhiteSpace(location))
+            _location = location;
+    }
+
+    public int GetEstablishedYear() => _establishedYear;
+
+    public void SetEstablishedYear(int year)
+    {
+        if (year > 1800)
+            _establishedYear = year;
+    }
+
+    public string Describe()
+    {
+        return $"Завод «{_name}», расположен: {_location}, основан в {_establishedYear} году.";
+    }
+}
