b5df2312ad9e1dc8ef45e560c5a87df632e66eac
Shaulouski_Stanislau <Shavlovsijstanislav3211@mail.ru>
2026-02-27 20:06:01 +0100

    Классы производственного заказа и проверки качества

---
+++ LabWork1/ProductionOrder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWork1
+{
+    public class ProductionOrder
+    {
+        private int _id;                    
+        private string _orderNumber;         
+        private int _productTypeId;          
+        private int _quantity;              
+        private string _plannedStartDate;   
+        private string _plannedEndDate;     
+
+
+        private string _status;
+
+        public ProductionOrder(int id, string orderNumber, int productTypeId, int quantity,
+                               string startDate, string endDate)
+        {
+            _id = id;
+            _orderNumber = orderNumber;
+            _productTypeId = productTypeId;
+            _quantity = quantity;
+            _plannedStartDate = startDate;
+            _plannedEndDate = endDate;
+            _status = "ЗАПЛАНИРОВАН";
+        }
+
+        public int GetId() => _id;
+        public string GetOrderNumber() => _orderNumber;
+        public int GetProductTypeId() => _productTypeId;
+        public int GetQuantity() => _quantity;
+        public string GetPlannedStartDate() => _plannedStartDate;
+        public string GetPlannedEndDate() => _plannedEndDate;
+        public string GetStatus() => _status;
+
+        public void Start()
+        {
+            if (_status == "ЗАПЛАНИРОВАН")
+                _status = "В РАБОТЕ";
+        }
+
+        public void Complete()
+        {
+            if (_status == "В РАБОТЕ")
+                _status = "ЗАВЕРШЁН";
+        }
+
+        public bool IsInProgress() => _status == "В РАБОТЕ";
+    }
+
+}
---
+++ LabWork1/QualityCheck.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWork1
+{
+    public class QualityCheck
+    {
+        private int _id;
+        private int _orderId;
+        private int _productTypeId;
+        private string _inspectorName;
+        private string _result;
+        private int _defectsCount;
+
+        public QualityCheck(int id, int orderId, int productTypeId,
+                            string inspectorName, string result, int defects)
+        {
+            _id = id;
+            _orderId = orderId;
+            _productTypeId = productTypeId;
+            _inspectorName = inspectorName;
+            _result = result;
+            _defectsCount = defects;
+        }
+
+        public int GetId() => _id;
+        public int GetOrderId() => _orderId;
+        public int GetProductTypeId() => _productTypeId;
+        public string GetInspectorName() => _inspectorName;
+        public string GetResult() => _result;
+        public int GetDefectsCount() => _defectsCount;
+
+        public bool IsPassed() => _result == "PASSED";
+
+        public double DefectRate(int totalChecked)
+        {
+            if (totalChecked <= 0) return 0;
+            return (double)_defectsCount / totalChecked;
+        }
+    }
+}
