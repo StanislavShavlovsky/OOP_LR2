312e5ff5f4005210c20726a130606578954d4b7b
Shaulouski_Stanislau <Shavlovsijstanislav3211@mail.ru>
2026-02-27 19:19:52 +0100

    Создание класса Workshop

---
+++ LabWork1/Workshop.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWork1
+{
+    public class Workshop
+    {
+        private int _id;
+        private string _code;
+        private string _name;
+        private int _factoryId;
+        private double _areaSqM;
+
+        public Workshop(int id, string code, string name, int factoryId, double areaSqM)
+        {
+            _id = id;
+            _code = code;
+            _name = name;
+            _factoryId = factoryId;
+            _areaSqM = areaSqM;
+        }
+
+        public int GetId() => _id;
+        public string GetCode() => _code;
+        public string GetName() => _name;
+
+        public void SetName(string name)
+        {
+            if (!string.IsNullOrWhiteSpace(name))
+                _name = name;
+        }
+
+        public int GetFactoryId() => _factoryId;
+        public double GetAreaSqM() => _areaSqM;
+
+        public bool IsLarge(double threshold)
+        {
+            return _areaSqM >= threshold;
+        }
+    }
+
+}
