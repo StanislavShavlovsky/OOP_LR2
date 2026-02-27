18201f6b48b0ffc93a9976a95005ae988c045a8c
Shaulouski_Stanislau <Shavlovsijstanislav3211@mail.ru>
2026-02-27 22:52:54 +0100

    параметрический полиморфизм

---
+++ LabWork1/Repository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LabWork1
+{
+    public class Repository<T>
+    {
+        private List<T> _items = new List<T>();
+
+        public void Add(T item) => _items.Add(item);
+        public IEnumerable<T> GetAll() => _items;
+    }
+}
+
