bc6daaa07670f3fdeafcf5a198717dbb9a0aa27b
Shaulouski_Stanislau <Shavlovsijstanislav3211@mail.ru>
2026-02-27 19:50:42 +0100

    создание класса смен и типов продукции выпускоемой заводом

---
+++ LabWork1/ProductType.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWork1
+{
+    public class ProductType
+    {
+        private int _id;
+        private string _code;
+        private string _name;
+        private string _description;
+        private double _standardCycleTimeMin;
+
+        public ProductType(int id, string code, string name, string description, double cycleTime)
+        {
+            _id = id;
+            _code = code;
+            _name = name;
+            _description = description;
+            _standardCycleTimeMin = cycleTime;
+        }
+
+        public int GetId() => _id;
+        public string GetCode() => _code;
+        public string GetName() => _name;
+
+        public void SetName(string name)
+        {
+            if (!string.IsNullOrWhiteSpace(name))
+                _name = name;
+        }
+
+        public string GetDescription() => _description;
+
+        public void SetDescription(string description)
+        {
+            if (!string.IsNullOrWhiteSpace(description))
+                _description = description;
+        }
+
+        public double GetStandardCycleTimeMin() => _standardCycleTimeMin;
+
+        public bool IsComplex(double threshold)
+        {
+            return _standardCycleTimeMin >= threshold;
+        }
+    }
+}
---
+++ LabWork1/Shift.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWork1
+{
+    public class Shift
+    {
+        private int _id;
+        private string _code;
+        private double _startHour;
+        private double _endHour;
+        private int _maxWorkers;
+
+        public Shift(int id, string code, double startHour, double endHour, int maxWorkers)
+        {
+            _id = id;
+            _code = code;
+            _startHour = startHour;
+            _endHour = endHour;
+            _maxWorkers = maxWorkers;
+        }
+
+        public int GetId() => _id;
+        public string GetCode() => _code;
+        public double GetStartHour() => _startHour;
+        public double GetEndHour() => _endHour;
+        public int GetMaxWorkers() => _maxWorkers;
+
+        public double DurationHours()
+        {
+            if (_endHour >= _startHour)
+                return _endHour - _startHour;
+
+            return (24 - _startHour) + _endHour;
+        }
+
+        public bool CanAcceptMore(int currentCount)
+        {
+            return currentCount < _maxWorkers;
+        }
+    }
+}
